665237d54672ed435a8df2ee2fb17a6590f91d3f
Bavikar <shital.r.bavikar@accenture.com>
2019-04-04 10:40:42 +0100

    final blog chanages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(
-<Provider store= {createStore(reducers, applyMiddleware(thunk))}>
+<Provider store= {createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
     <App/>
 </Provider>,
 document.getElementById('root'));
